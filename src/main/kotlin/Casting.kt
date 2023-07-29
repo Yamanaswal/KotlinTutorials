fb669acfbb067f8878aee0304fd51d111b5d1432
Yaman Aswal <yaman@appsquad.com>
2023-07-29 14:09:38 +0530

    before merge - 2

---
+++ src/main/kotlin/Casting.kt
@@ -0,0 +1,22 @@
+fun main() {
+
+    val circle = Circle()
+
+    //Type Checking.
+    if (circle is Circle) {
+
+    }
+
+    //Type Casting
+    val objs = arrayOf(Circle(), Player())
+
+    for (ob in objs) {
+        //Smart Casting
+        if (ob is Circle) {
+            ob.draw()
+            println(ob.radius)
+        } else {
+            (ob as Player).draw()
+        }
+    }
+}
--- src/main/kotlin/Interface.kt
+++ src/main/kotlin/Interface.kt
@@ -1,27 +1,30 @@
-fun main()
-{
+fun main() {
     dragObjects(arrayOf(Circle(), Player()))
 }
 
-fun dragObjects(list: Array<Draggable>)
-{
-    for(obj in list)
-    {
+fun dragObjects(list: Array<Draggable>) {
+    for (obj in list) {
         obj.drag()
     }
 }
 
-interface Draggable
-{
+interface Draggable {
     fun drag()
+    fun draw()
 }
 
-class Circle: Draggable
-{
+class Circle : Draggable {
+
+    val radius = 23
     override fun drag() = println("Circle is dragging")
+    override fun draw() {
+        println("Circle Draw...")
+    }
 }
 
-class Player: Draggable
-{
+class Player : Draggable {
     override fun drag() = println("Person is dragging")
+    override fun draw() {
+        println("Player Draw...")
+    }
 }
