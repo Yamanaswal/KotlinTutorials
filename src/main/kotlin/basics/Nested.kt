19caf11e2f0e25edf2795d4f7916e5fadeea9dcb
Yaman Aswal <yaman@appsquad.com>
2023-07-30 18:36:51 +0530

    kotlin basics completed

---
+++ src/main/kotlin/basics/Nested.kt
@@ -0,0 +1,30 @@
+package basics
+
+fun main() {
+    val ob = OuterClass()
+    println(ob.i)
+
+    val nested = OuterClass.NestedClass()
+    println(nested.test())
+
+    val outer = OuterClass()
+    val inner = outer.InnerClass()
+    println(inner.test())
+}
+
+class OuterClass {
+    var i = 100
+
+    class NestedClass() {
+
+        fun test() {
+            println("I am nested class")
+        }
+    }
+
+    inner class InnerClass {
+        fun test() {
+            println("I am nested class $i")
+        }
+    }
+}
